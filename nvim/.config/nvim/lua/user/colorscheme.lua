0763816c40f88e68b0d155857cbcaf629b2d2bef
ecklf <ecklf@icloud.com>
2022-09-13 14:48:54 +0200

    feat(nvim): conditionally apply colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,15 @@
 local colorscheme = "catppuccin"
 -- Flavours: latte, frappe, macchiato, mocha
-vim.g.catppuccin_flavour = "mocha"
+
+local theme_scheme = vim.fn.getenv("THEME_SCHEME")
+
+if theme_scheme == "light" then
+	vim.g.catppuccin_flavour = "latte"
+elseif theme_scheme == "dark" then
+	vim.g.catppuccin_flavour = "mocha"
+else
+	vim.g.catppuccin_flavour = "mocha"
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
