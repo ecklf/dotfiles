cb96f52580f483e91c484aa593f1bb67081a7407
Florentin Eckl <ecklf@icloud.com>
2024-07-25 10:16:34 +0200

    fix autoformat

--- nix/home/nvim/plugins/conform.lua
+++ nix/home/nvim/plugins/conform.lua
@@ -3,60 +3,73 @@ if not conform_status_ok then
 	return
 end
 
+local formatters = {
+	lua = { "stylua" },
+	python = { "isort", "black" },
+	javascript = { "prettier" },
+	typescript = { "prettier" },
+	javascriptreact = { "prettier" },
+	typescriptreact = { "prettier" },
+	css = { "prettier" },
+	html = { "prettier" },
+	sh = { "shfmt" },
+	toml = { "prettier" },
+	rust = { "rustfmt" },
+	go = { "goimports", "gofmt" },
+	nix = { "nixpkgs_fmt" },
+	terraform = { "terraform_fmt" },
+}
+
 conform.setup({
 	lsp_fallback = true,
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "isort", "black" },
-		javascript = { "prettier" },
-		typescript = { "prettier" },
-		javascriptreact = { "prettier" },
-		typescriptreact = { "prettier" },
-		css = { "prettier" },
-		html = { "prettier" },
-		sh = { "shfmt" },
-		toml = { "prettier" },
-		rust = { "rustfmt" },
-		go = { "goimports", "gofmt" },
-		nix = { "nixpkgs_fmt" },
-		terraform = { "terraform_fmt" },
+	formatters_by_ft = formatters,
+	formatters = {
+		[formatters.lua[1]] = {
+			---@diagnostic disable-next-line: unused-local
+			condition = function(self, ctx)
+				if string.match(ctx.dirname, "/vercel/proxy/") then
+					return false
+				end
+
+				return true
+			end,
+		},
 	},
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		conform.format({ bufnr = args.buf })
+	format_after_save = function(bufnr)
+		-- Disable with a global or buffer-local variable
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 1000, lsp_fallback = true, async = true, formatters_by_ft = formatters }
 	end,
-})
-
-vim.api.nvim_create_user_command("Format", function(args)
-	local range = nil
-	if args.count ~= -1 then
-		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-		range = {
-			start = { args.line1, 0 },
-			["end"] = { args.line2, end_line:len() },
-		}
-	end
-	conform.format({ async = true, lsp_fallback = true, range = range })
-end, { range = true })
-
-vim.api.nvim_create_user_command("FormatDisable", function(args)
-	if args.bang then
-		-- FormatDisable! will disable formatting just for this buffer
-		vim.b.disable_autoformat = true
-	else
-		vim.g.disable_autoformat = true
-	end
-end, {
-	desc = "Disable autoformat-on-save",
-	bang = true,
-})
-
-vim.api.nvim_create_user_command("FormatEnable", function()
-	vim.b.disable_autoformat = false
-	vim.g.disable_autoformat = false
-end, {
-	desc = "Re-enable autoformat-on-save",
+	init = function()
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			conform.format({ async = true, lsp_fallback = true, range = range })
+		end, { range = true })
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
 })
