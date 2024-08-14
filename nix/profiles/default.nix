243df5744e22def6fabfa58cadde3a97fba4f66c
Florentin Eckl <ecklf@icloud.com>
2024-08-14 14:32:51 +0200

    increase old space size

--- nix/profiles/default.nix
+++ nix/profiles/default.nix
@@ -179,6 +179,7 @@
       enable = true;
       profileExtra = ''
         eval "$(/opt/homebrew/bin/brew shellenv)"
+        export NODE_OPTIONS=--max-old-space-size=8096
       '';
       prezto = {
         enable = true;
