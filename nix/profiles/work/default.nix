57d3bf0f75fc9ede24aa2105011d9c8cbd6e3066
Florentin Eckl <ecklf@icloud.com>
2024-11-21 22:27:59 +0100

    update package descriptions

--- nix/profiles/work/default.nix
+++ nix/profiles/work/default.nix
@@ -1,14 +1,14 @@
 ({ config, profile, lib, pkgs, nixpkgs-master, ... }: {
   home = {
     packages = [
-      pkgs.master.go-task
-      pkgs.goreleaser
-      pkgs.aws-iam-authenticator
-      pkgs.lua51Packages.busted
-      pkgs.lua51Packages.lua
-      pkgs.lua51Packages.luacheck
-      pkgs.lua51Packages.luafilesystem
-      pkgs.lua51Packages.luarocks
+      pkgs.aws-iam-authenticator # AWS IAM credentials for Kubernetes authentication
+      pkgs.goreleaser # Deliver Go binaries as fast and easily as possible
+      pkgs.lua51Packages.busted # Elegant Lua unit testing
+      pkgs.lua51Packages.lua # Powerful, fast, lightweight, embeddable scripting language
+      pkgs.lua51Packages.luacheck # A static analyzer and a linter for Lua
+      pkgs.lua51Packages.luafilesystem # File System Library for the Lua Programming Language
+      pkgs.lua51Packages.luarocks # A package manager for Lua
+      pkgs.master.go-task # A task runner / simpler Make alternative written in Go
     ];
   };
 
