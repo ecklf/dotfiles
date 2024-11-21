33b11fd4b2c510ccf48627796fb4090462a1cebc
Florentin Eckl <ecklf@icloud.com>
2024-11-21 20:22:09 +0100

    update pass

--- nix/machines/snowflake/default.nix
+++ nix/machines/snowflake/default.nix
@@ -39,7 +39,7 @@
   # Pick only one of the below networking options.
   networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
   networking.wireless.networks = {
-    "WIFI".psk = "PASS";
+    "placeholderssid".psk = "placeholderpassword";
   };
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
