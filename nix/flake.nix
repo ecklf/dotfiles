66933ca3e378b9cf81ad1593b26d655ee53013d2
Florentin Eckl <ecklf@icloud.com>
2024-11-22 11:44:04 +0100

    remove nixos hardware input

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,17 +1,20 @@
 {
   description = "NixOS and Darwin flakes";
   inputs = {
-    nixos-hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nur.url = "github:nix-community/NUR";
     # Controls macOS system level software and settings including fonts
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # Manages configs links and home directory
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -19,7 +22,6 @@
     , nixpkgs
     , nixpkgs-stable
     , nixpkgs-master
-    , nixos-hardware
     , nur
     , darwin
     , home-manager
