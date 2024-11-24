b3fc12fbacf4b7feee5166170f9ee39296011e3c
Florentin Eckl <ecklf@icloud.com>
2024-11-24 16:10:08 +0100

    map capslock to esc

--- nix/machines/snowflake/default.nix
+++ nix/machines/snowflake/default.nix
@@ -150,6 +150,19 @@
     };
   };
 
+  services.interception-tools = {
+    enable = true;
+    plugins = with pkgs; [
+      interception-tools-plugins.caps2esc
+    ];
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
+
   system.stateVersion = "24.05";
 }
 
