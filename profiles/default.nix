389da9749af21f1f7771902f81d35be6a3840fb1
Florentin Eckl <ecklf@icloud.com>
2024-11-27 23:35:21 +0100

    add stdenv gates

--- profiles/default.nix
+++ profiles/default.nix
@@ -1,8 +1,11 @@
 ({
+  lib,
   profile,
   pkgs,
   ...
-}: {
+}: let
+  inherit (pkgs.stdenv) isDarwin;
+in {
   imports = [
     ../home/bat
     ../home/eza
@@ -19,19 +22,27 @@
 
   home = {
     stateVersion = "24.05";
-    sessionVariables = {
-      PAGER = "less";
-      CLICLOLOR = 1;
-      EDITOR = "nvim";
-      VISUAL = "nvim";
-      GSETTINGS_SCHEMA_DIR = "/opt/homebrew/share/glib-2.0/schemas";
-    };
-    file = {
-      ".config/ghostty/config".source = ./../config/ghostty/config;
-      ".config/iterm2/com.googlecode.iterm2.plist".source = ./../config/iterm2/com.googlecode.iterm2.plist;
-      ".config/iterm2/nordic_light.itermcolors".source = ./../config/iterm2/nordic_light.itermcolors;
-      ".config/iterm2/nordic_dark.itermcolors".source = ./../config/iterm2/nordic_dark.itermcolors;
-    };
+    sessionVariables =
+      {
+        PAGER = "less";
+        CLICLOLOR = 1;
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+      }
+      // lib.optionalAttrs isDarwin
+      {
+        GSETTINGS_SCHEMA_DIR = "/opt/homebrew/share/glib-2.0/schemas";
+      };
+    file =
+      {}
+      // lib.optionalAttrs
+      isDarwin
+      {
+        ".config/ghostty/config".source = ./../config/ghostty/config;
+        ".config/iterm2/com.googlecode.iterm2.plist".source = ./../config/iterm2/com.googlecode.iterm2.plist;
+        ".config/iterm2/nordic_light.itermcolors".source = ./../config/iterm2/nordic_light.itermcolors;
+        ".config/iterm2/nordic_dark.itermcolors".source = ./../config/iterm2/nordic_dark.itermcolors;
+      };
     packages = [
       # pkgs.cfssl # Cloudflare's PKI and TLS toolkit
       # pkgs.darwin.libiconv # required for -liconv mitmproxy compilation
@@ -187,11 +198,13 @@
       '';
       prezto = {
         enable = true;
-        pmodules = [
-          "git"
-          "osx"
-          "homebrew"
-        ];
+        pmodules = lib.flatten ([
+            "git"
+          ]
+          ++ lib.optional isDarwin [
+            "osx"
+            "homebrew"
+          ]);
       };
       sessionVariables = {
         NEXT_TELEMETRY_DISABLED = 1;
@@ -199,72 +212,77 @@
         PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
         PUPPETEER_EXECUTABLE_PATH = "which chromium";
       };
-      shellAliases = {
-        # Abbreviations
-        c = "clear";
-        m = "make";
-        n = "nvim";
-        o = "open";
-        # General
-        tree1 = "exa --tree --level=1 --icons .";
-        tree2 = "exa --tree --level=2 --icons .";
-        tree3 = "exa --tree --level=3 --icons .";
-        diskusage = "sudo smartctl --all /dev/disk0";
-        # Common
-        dots = "vim ~/dotfiles";
-        untar = "tar -zxvf ";
-        backup = ''[ -d '/Volumes/MBP Backup' ] && mkdir /Volumes/MBP\ Backup/''$(date +%F) && rsync -av --exclude='Applications' --exclude='Library' --exclude='Trash' --exclude='node_modules' --exclude='.*' /Users/''${WHOAMI}/ /Volumes/MBP\ Backup/''$(date +%F)'';
-        # Crypto
-        sha = "shasum -a 256 ";
-        gen_pass = "gpg --gen-random --armor 0 32";
-        # Git
-        noflakepls = "git commit --allow-empty -m 'force: CI'";
-        nogpgflakepls = "gpgconf --kill gpg-agent && gpg-connect-agent /bye";
-        rgpg = "gpg-connect-agent killagent /bye && gpg-connect-agent /bye";
-        # Network
-        ping = "ping -c 5";
-        jcurl = ''curl -H 'Content-Type: application/json' "''$@"'';
-        wget = "wget -c ";
-        ipi = "ipconfig getifaddr en0";
-        ipe = "curl ifconfig.me";
-        # Media
-        to_webp = ''for i in *.* ; do cwebp -q 80 "$i" -o "''${i%.*}.webp" ; done'';
-        to_png = ''for i in *.* ; do convert "''$i" "''${i%.*}.png" ; done'';
-        to_mp4 = ''for i in *.* ; do ffmpeg -i "''$i" "''${i%.*}-o.mp4" ; done'';
-        dl = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 ''$1";
-        dl_mp3 = "yt-dlp --extract-audio --audio-format mp3 ''$1";
-        # Misc
-        tb = "nc termbin.com 9999";
-        wttr = "curl v2.wttr.in/Munich";
-        ms = "meilisearch --db-path ~/data.ms";
-        # Docker
-        dip = "docker image prune";
-        dls = "docker ps";
-        dlsa = "docker ps -a";
-        dcu = "docker-compose up";
-        dcd = "docker-compose down";
-        dcdv = "docker-compose down -v";
-        # lnjava = "sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk";
-        # Node
-        p = "pnpm";
-        nls = "npm list -g --depth 0";
-        yat = "yarn add -D postcss tailwindcss @tailwindcss/forms @tailwindcss/typography @tailwindcss/aspect-ratio";
-        # reshim = "asdf reshim nodejs";
-        init_cz = "commitizen init cz-conventional-changelog --yarn --dev --exact";
-        vscode_ls = "code --list-extensions | xargs -L 1 echo code --install-extension";
-        # React
-        rni = "npx react-native run-ios";
-        rna = "npx react-native run-android";
-        # React Native
-        adbr = "adb reverse tcp:8081 tcp:8081"; # reconnect metro bundler
-        iosd = "xcrun xctrace list devices"; # shows iOS devices
-        # Terraform
-        tf = "tf $1";
-        # Credentials
-        # google_cred="export GOOGLE_APPLICATION_CREDENTIALS='~/service-account.json'";
-        # avd="cd ~/Library/Android/sdk/emulator/ && ./emulator -avd Pixel_3_API_29";
-        # chrome_dbg="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222";
-      };
+      shellAliases =
+        {
+          # Abbreviations
+          c = "clear";
+          m = "make";
+          n = "nvim";
+          # General
+          tree1 = "exa --tree --level=1 --icons .";
+          tree2 = "exa --tree --level=2 --icons .";
+          tree3 = "exa --tree --level=3 --icons .";
+          diskusage = "sudo smartctl --all /dev/disk0";
+          # Common
+          dots = "vim ~/dotfiles";
+          untar = "tar -zxvf ";
+          backup = ''[ -d '/Volumes/MBP Backup' ] && mkdir /Volumes/MBP\ Backup/''$(date +%F) && rsync -av --exclude='Applications' --exclude='Library' --exclude='Trash' --exclude='node_modules' --exclude='.*' /Users/''${WHOAMI}/ /Volumes/MBP\ Backup/''$(date +%F)'';
+          # Crypto
+          sha = "shasum -a 256 ";
+          gen_pass = "gpg --gen-random --armor 0 32";
+          # Git
+          noflakepls = "git commit --allow-empty -m 'force: CI'";
+          nogpgflakepls = "gpgconf --kill gpg-agent && gpg-connect-agent /bye";
+          rgpg = "gpg-connect-agent killagent /bye && gpg-connect-agent /bye";
+          # Network
+          ping = "ping -c 5";
+          jcurl = ''curl -H 'Content-Type: application/json' "''$@"'';
+          wget = "wget -c ";
+          ipi = "ipconfig getifaddr en0";
+          ipe = "curl ifconfig.me";
+          # Media
+          to_webp = ''for i in *.* ; do cwebp -q 80 "$i" -o "''${i%.*}.webp" ; done'';
+          to_png = ''for i in *.* ; do convert "''$i" "''${i%.*}.png" ; done'';
+          to_mp4 = ''for i in *.* ; do ffmpeg -i "''$i" "''${i%.*}-o.mp4" ; done'';
+          dl = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 ''$1";
+          dl_mp3 = "yt-dlp --extract-audio --audio-format mp3 ''$1";
+          # Misc
+          tb = "nc termbin.com 9999";
+          wttr = "curl v2.wttr.in/Munich";
+          ms = "meilisearch --db-path ~/data.ms";
+          # Docker
+          dip = "docker image prune";
+          dls = "docker ps";
+          dlsa = "docker ps -a";
+          dcu = "docker-compose up";
+          dcd = "docker-compose down";
+          dcdv = "docker-compose down -v";
+          # lnjava = "sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk";
+          # Node
+          p = "pnpm";
+          nls = "npm list -g --depth 0";
+          yat = "yarn add -D postcss tailwindcss @tailwindcss/forms @tailwindcss/typography @tailwindcss/aspect-ratio";
+          # reshim = "asdf reshim nodejs";
+          init_cz = "commitizen init cz-conventional-changelog --yarn --dev --exact";
+          vscode_ls = "code --list-extensions | xargs -L 1 echo code --install-extension";
+          # React
+          rni = "npx react-native run-ios";
+          rna = "npx react-native run-android";
+          # React Native
+          adbr = "adb reverse tcp:8081 tcp:8081"; # reconnect metro bundler
+          # Terraform
+          tf = "tf $1";
+          # Kubernetes
+          k = "kubectl";
+          # Credentials
+          # google_cred="export GOOGLE_APPLICATION_CREDENTIALS='~/service-account.json'";
+          # avd="cd ~/Library/Android/sdk/emulator/ && ./emulator -avd Pixel_3_API_29";
+          # chrome_dbg="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222";
+        }
+        // lib.optionalAttrs isDarwin {
+          o = "open";
+          iosd = "xcrun xctrace list devices"; # shows iOS devices
+        };
     };
   };
 })
