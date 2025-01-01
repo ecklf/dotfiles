b243e680c6ea6fa7e26d01aba77abc58c6a147ad
Florentin Eckl <ecklf@icloud.com>
2025-01-01 10:42:05 +0100

    remove dbs

--- machines/omega/default.nix
+++ machines/omega/default.nix
@@ -1,8 +1,4 @@
-({
-  pkgs,
-  username,
-  ...
-}: let
+({...}: let
   isWorkMachine = false;
 in {
   homebrewModules = {
@@ -22,40 +18,5 @@ in {
     language = true;
     wine = true;
     game = true;
-  };
-
-  activationScriptModules.extraPreActivationScripts = [
-    ''
-      if [ ! -d "/var/lib/postgresql/" ]; then
-        echo "creating PostgreSQL data directory..."
-        sudo mkdir -m 750 -p /var/lib/postgresql/
-        chown -R ${username}:staff /var/lib/postgresql/
-      fi
-      if [ ! -d "/var/lib/redis/" ]; then
-        echo "creating Redis data directory..."
-        sudo mkdir -m 750 -p /var/lib/redis/
-        chown -R ${username}:staff /var/lib/redis/
-      fi
-    ''
-  ];
-
-  services = {
-    postgresql = {
-      enable = true;
-      initdbArgs = ["-U ${username}" "--pgdata=/var/lib/postgresql/15" "--auth=trust" "--no-locale" "--encoding=UTF8"];
-      package = pkgs.postgresql_15;
-      # TODO(ecklf) automate this
-      # psql postgres -c "CREATE USER postgres WITH PASSWORD 'postgres';"
-      # psql postgres -c "ALTER USER postgres WITH SUPERUSER;"
-      # createdb asdf
-    };
-    redis = {
-      enable = true;
-    };
-  };
-
-  launchd.user.agents.postgresql.serviceConfig = {
-    StandardErrorPath = "/tmp/postgres.error.log";
-    StandardOutPath = "/tmp/postgres.log";
   };
 })
