8285b99d48ff36d3d39d97d16e0150df1b108c18
Florentin Eckl <ecklf@icloud.com>
2025-03-09 15:22:55 +0100

    update soma

--- machines/soma/configuration.nix
+++ machines/soma/configuration.nix
@@ -1,14 +1,20 @@
 {
-  modulesPath,
-  lib,
   pkgs,
+  username,
+  timezone,
+  modulesPath,
   ...
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
+    ./hardware-configuration.nix
   ];
+
+  time.timeZone = timezone;
+  boot.tmp.cleanOnBoot = true;
+
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
@@ -16,16 +22,12 @@
     efiInstallAsRemovable = true;
   };
   services.openssh.enable = true;
-
-  environment.systemPackages = map lib.lowPrio [
-    pkgs.curl
-    pkgs.gitMinimal
-  ];
+  users.defaultUserShell = pkgs.zsh;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    # change this to your ssh key
-    "CHANGE"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzp3OPA8XUVrapGPaL4plEuVE9wwhevUkKbtynXrYUZ ecklf@icloud.com"
   ];
+  users.users."${username}".openssh.authorizedKeys.keys = [];
 
   system.stateVersion = "24.05";
 }
