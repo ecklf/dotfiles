d29443e87ddf9f3746f5bb0c4fe2096ceec3b2aa
Florentin Eckl <ecklf@icloud.com>
2025-03-11 17:53:19 +0000

    add k3s config

--- machines/soma/default.nix
+++ machines/soma/default.nix
@@ -41,5 +41,20 @@
     ${username} ALL=(ALL) NOPASSWD:ALL
   '';
 
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  services.k3s.extraFlags = toString [
+    # "--debug"
+    "--write-kubeconfig-mode '0644'"
+  ];
+
   system.stateVersion = "24.05";
 }
