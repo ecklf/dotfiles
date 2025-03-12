8a00e4636aae1e60586d984f01e9e51507973222
Florentin Eckl <ecklf@icloud.com>
2025-03-12 17:32:52 +0000

    expose http(s) ports

--- machines/soma/default.nix
+++ machines/soma/default.nix
@@ -44,6 +44,8 @@
   '';
 
   networking.firewall.allowedTCPPorts = [
+    80
+    443
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
     # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
