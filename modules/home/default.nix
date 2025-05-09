427d9e4e1f3d2d91139a2f994ea165be980e8016
Florentin Eckl <ecklf@icloud.com>
2025-05-09 11:44:21 +0200

    tweak symlink

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -75,6 +75,7 @@ in {
         isDarwin
         {
           ".config/ghostty/config".source = ./config/ghostty/config;
+          ".config/ghostty/themes".source = ./config/ghostty/themes;
           ".config/iterm2/com.googlecode.iterm2.plist".source = ./config/iterm2/com.googlecode.iterm2.plist;
           ".config/iterm2/nordic_light.itermcolors".source = ./config/iterm2/nordic_light.itermcolors;
           ".config/iterm2/nordic_dark.itermcolors".source = ./config/iterm2/nordic_dark.itermcolors;
