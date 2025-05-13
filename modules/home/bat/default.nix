ac57146937b725f85429f817231644bdd899af0c
Florentin Eckl <ecklf@icloud.com>
2025-05-13 22:03:09 +0200

    bat pkgs

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -1,8 +1,17 @@
-_: {
+({pkgs, ...}: {
   programs = {
     bat = {
       enable = true;
       config.theme = "TwoDark";
+      extraPackages = with pkgs.bat-extras; [batdiff batman batgrep batwatch];
+      # extraOptions = [
+      #   "--style=plain"
+      #   "--color=always"
+      #   "--decorations=always"
+      #   "--paging=always"
+      #   "--theme=TwoDark"
+      #   "--terminal-width=80"
+      # ];
     };
   };
-}
+})
