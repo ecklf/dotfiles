01dc2a186f467846fcf70820442423d29e2028ac
Florentin Eckl <ecklf@icloud.com>
2025-05-13 22:02:40 +0200

    fix zen

--- modules/system/darwin/homebrew.nix
+++ modules/system/darwin/homebrew.nix
@@ -175,7 +175,7 @@
           "raycast"
           "vivaldi"
           "vlc"
-          "zen"
+          "zen-browser"
         ]
         ++ lib.optional (config.homebrewModules.developer && lib.optional system == "aarch64-darwin") [
           "lm-studio"
