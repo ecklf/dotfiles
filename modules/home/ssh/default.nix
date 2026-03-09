5956b96e0079f71dbdbb52368d5fc53ffea5b463
Florentin Eckl <ecklf@icloud.com>
2026-03-09 10:51:43 +0100

    fix: ssh darwin

--- modules/home/ssh/default.nix
+++ modules/home/ssh/default.nix
@@ -1,10 +1,15 @@
-{...}: {
+{pkgs, ...}: let
+  isDarwin = pkgs.stdenv.isDarwin;
+in {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    extraConfig = ''
-      IgnoreUnknown AddKeysToAgent,UseKeychain
-    '';
+    extraConfig =
+      if isDarwin
+      then ''
+        IgnoreUnknown AddKeysToAgent,UseKeychain
+      ''
+      else "";
     includes = [
       "~/.orbstack/ssh/config"
       "~/.colima/ssh_config"
@@ -21,15 +26,17 @@
         controlPath = "~/.ssh/master-%r@%h:%p"; # Socket path for shared connections
         controlPersist = "10m"; # Keep shared connections alive for 10 minutes
         setEnv = {TERM = "xterm-256color";}; # Set terminal type for better compatibility
-        # addKeysToAgent = "yes"; # Automatically add keys to SSH agent (commented - can be annoying)
-        # identitiesOnly = true; # Only use explicitly configured keys (commented - can break some setups)
-        # strictHostKeyChecking = "ask"; # Default behavior (commented - already default)
       };
       "github.com" = {
-        extraOptions = {
-          AddKeysToAgent = "yes";
-          UseKeychain = "yes";
-        };
+        extraOptions =
+          {
+            AddKeysToAgent = "yes";
+          }
+          // (
+            if isDarwin
+            then {UseKeychain = "yes";}
+            else {}
+          );
         identityFile = "~/.ssh/id_ed25519";
       };
     };
