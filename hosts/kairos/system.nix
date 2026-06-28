f235f233ef7144c241bf079beba59589efec84ef
Florentin Eckl <ecklf@icloud.com>
2026-06-28 23:41:59 +0200

    kairos: fix

--- hosts/kairos/system.nix
+++ hosts/kairos/system.nix
@@ -65,6 +65,13 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:8000";
         proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Real-IP $remote_addr;
+        '';
       };
     };
   };
