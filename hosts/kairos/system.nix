873dfc8d2c505a84ebb67553f4f1e621fa536953
Florentin Eckl <ecklf@icloud.com>
2026-06-28 23:22:11 +0200

    kairos: add duckdns

--- hosts/kairos/system.nix
+++ hosts/kairos/system.nix
@@ -1,4 +1,4 @@
-{modulesPath, username, ...}: {
+{modulesPath, username, timezone, config, ...}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
 
   boot.loader.grub.device = "/dev/vda";
@@ -15,6 +15,56 @@
     daemon.settings = {
       features = {
         buildkit = true;
+      };
+    };
+  };
+
+  time.timeZone = timezone;
+
+  sops = {
+    defaultSopsFile = ../../lib/secrets/networks.yaml;
+    age = {
+      sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+      keyFile = "/home/${username}/.config/sops/age/keys.txt";
+      generateKey = true;
+    };
+    secrets.acme_yun = {};
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [80 443];
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "ecklf@icloud.com";
+      dnsPropagationCheck = true;
+    };
+    certs."xiangji.duckdns.org" = {
+      dnsProvider = "duckdns";
+      environmentFile = config.sops.secrets.acme_yun.path;
+      dnsPropagationCheck = true;
+      extraLegoFlags = ["--cert.timeout" "300"];
+    };
+  };
+
+  users.users.nginx.extraGroups = ["acme"];
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+
+    virtualHosts."xiangji.duckdns.org" = {
+      forceSSL = true;
+      useACMEHost = "xiangji.duckdns.org";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8000";
+        proxyWebsockets = true;
       };
     };
   };
