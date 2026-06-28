35e242bc3971615f62192c3557da8abbb809eeb9
Florentin Eckl <ecklf@icloud.com>
2026-06-28 20:38:40 +0200

    kairos: rm nextcloud

--- hosts/kairos/system.nix
+++ hosts/kairos/system.nix
@@ -1,33 +1,5 @@
-{
-  modulesPath,
-  hostname,
-  pkgs,
-  ...
-}: {
+{modulesPath, ...}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-
-  networking.hostName = hostname;
-  networking.firewall.allowedTCPPorts = [80 443];
-
-  services.postgresql.enable = true;
-  services.redis.servers.nextcloud.enable = true;
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud32;
-    https = false;
-    hostName = hostname;
-    config = {
-      dbtype = "pgsql";
-      adminuser = "admin";
-      adminpassFile = "/var/lib/nextcloud-admin-pass";
-    };
-    database.createLocally = true;
-    configureRedis = true;
-    settings = {
-      trusted_domains = [hostname];
-      default_phone_region = "US";
-    };
-  };
 
   boot.loader.grub.device = "/dev/vda";
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
