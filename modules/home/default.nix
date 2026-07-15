6362472649da3c9165cba069f6659c57702657ba
Florentin Eckl <ecklf@icloud.com>
2026-07-15 11:24:13 +0200

    chore: extract commented pkgs into separate module

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -74,6 +74,11 @@ in {
           type = lib.types.bool;
           description = "Install embedded development tools";
         };
+        testing = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+          description = "Install testing software";
+        };
         extraPackages = lib.mkOption {
           type = lib.types.listOf lib.types.package;
           default = [];
@@ -125,26 +130,19 @@ in {
         config.home.modules.extraPackages
         ++ lib.optional isDarwin [
           pkgs.mas # Mac App Store command line interface
-          # pkgs.gnupg # Managed by programs.gpg
           pkgs.gnused # GNU sed, a batch stream editor
           pkgs.gnutls # The GNU Transport Layer Security Library
           pkgs.gawk # GNU implementation of the Awk programming language
-          # pkgs.lima # Linux virtual machines (on macOS, in most cases)
-          # pkgs.mitmproxy # Man-in-the-middle proxy
-          # pkgs.darwin.libiconv # required for -liconv mitmproxy compilation
         ]
         ++ lib.optional (config.home.modules.minimal && system == "aarch64-darwin") [
           # Fails to compile on x86_64
           pkgs.fclones # Efficient Duplicate File Finder and Remover
         ]
         ++ lib.optional config.home.modules.minimal [
-          # pkgs.master.gitui # Blazing fast terminal-ui for Git written in Rust
-          # pkgs.nixpkgs-fmt # Nix code formatter for nixpkgs
           pkgs.ack # A grep-like tool tailored to working with large trees of source code
           pkgs.alejandra # Nix code formatter
           pkgs.curl # A command line tool for transferring files with URL syntax
           pkgs.xh # Fast tool for sending http requests
-          # pkgs.direnv # Managed by programs.direnv
           pkgs.eza # A modern, maintained replacement for ls
           pkgs.fd # Suite of speech signal processing tools
           pkgs.fnm # Fast and simple Node.js version manager
@@ -153,8 +151,6 @@ in {
           pkgs.git-lfs # Git extension for versioning large files
           pkgs.git-trim # Automatically trims your branches whose tracking remote refs are merged or gone
           pkgs.htop # An interactive process viewer for Linux, with vim-style keybindings
-          # pkgs.jq # Managed by programs.jq
-          # pkgs.lazygit # Managed by programs.lazygit
           pkgs.less # A more advanced file pager than 'more'
           pkgs.go-task # A task runner / simpler Make alternative written in Go
           pkgs.most # A pager with syntax highlighting for man pages
@@ -164,7 +160,6 @@ in {
           pkgs.pv # Tool for monitoring the progress of data through a pipeline
           pkgs.master.rclone # Command line program to sync files and directories to and from major cloud storage
           pkgs.repgrep # Interactive replacer for ripgrep that makes it easy to find and replace across files on the command line
-          # pkgs.ripgrep # Managed by programs.ripgrep
           pkgs.rsync # Fast incremental file transfer utility
           pkgs.stow # A tool for managing the installation of multiple software packages in the same run-time directory tree
           pkgs.subversion # A version control system intended to be a compelling replacement for CVS in the open source community
@@ -184,7 +179,6 @@ in {
           pkgs.ntfy # Utility for sending notifications, on demand and when commands finish
           # Development
           pkgs.cargo-nextest # Next-generation test runner for Rust projects
-          # pkgs.master.cargo-watch # A Cargo subcommand for watching over Cargo project's source
           pkgs.cargo-zigbuild # A tool to compile Cargo projects with zig as the linker
           pkgs.cargo-binstall # A Cargo subcommand to install Rust binaries
           pkgs.cargo-binutils # Cargo integration for LLVM's binary utilities
@@ -225,7 +219,6 @@ in {
           pkgs.age # Modern encryption tool with small explicit keys
           pkgs.ast-grep # A tool to search for AST nodes in source code
           pkgs.certstrap # Tools to bootstrap CAs, certificate requests, and signed certificates
-          # pkgs.direnv # Managed by programs.direnv
           pkgs.fx # Terminal JSON viewer
           pkgs.glow # Render markdown on the CLI, with pizzazz!
           pkgs.gomplate # A flexible commandline tool for template rendering
@@ -241,11 +234,9 @@ in {
           pkgs.sea-orm-cli #  Command line utility for SeaORM
           # GitHub
           pkgs.act # Run your GitHub Actions locally
-          # pkgs.gh # Managed by programs.gh
           pkgs.serie # Rich git commit graph in your terminal, like magic
           # Networking / Monitoring / Benchmarking
           pkgs.atac # Simple API client (postman like) in your terminal
-          # pkgs.btop # Managed by programs.btop
           pkgs.frp # A fast reverse proxy to help you expose a local server behind a NAT or firewall to the internet
           pkgs.glances # Cross-platform curses-based monitoring tool
           (pkgs.master.httpstat.overrideAttrs (old: {
@@ -262,7 +253,6 @@ in {
           pkgs.plow # A high-performance HTTP benchmarking tool that includes a real-time web UI and terminal display
           pkgs.ngrok # A Python wrapper for ngrok
           pkgs.nmap # A free and open source utility for network discovery and security auditing
-          # pkgs.s-tui # Stress-Terminal UI monitoring tool - disabled because marked as broken
           pkgs.smartmontools # Tools for monitoring the health of hard drives
           pkgs.speedtest-cli # Command line interface for testing internet bandwidth using speedtest.net
           pkgs.subfinder # Subdomain discovery tool
@@ -271,7 +261,6 @@ in {
           # Cloud
           pkgs.awscli2 # Unified tool to manage your AWS services
           pkgs.google-cloud-sdk # Tools for the google cloud platform
-          # pkgs.nodePackages_latest.aws-cdk # CDK Toolkit, the command line tool for CDK apps
           pkgs.terraform # OpenTofu, Terraform, Terragrunt and Atmos version manager written in Go
           pkgs.terragrunt # Thin wrapper for Terraform that provides extra tools for working with multiple Terraform modules
           # Media
@@ -288,9 +277,6 @@ in {
           pkgs.tui-journal # A terminal-based journal application
           pkgs.wikiman # Offline search engine for manual pages
           pkgs.tealdeer # A very fast implementation of tldr in Rust
-          # Yubikey
-          # pkgs.yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
-          # pkgs.yubikey-personalization # A library and command line tool to personalize YubiKeys
         ]
         ++ lib.optional config.home.modules.personal [
           pkgs.stripe-cli # A command-line tool for Stripe
@@ -316,29 +302,12 @@ in {
           # Hipster editors
           pkgs.emacs # The extensible, customizable GNU text editor
           pkgs.helix # A post-modern modal text editor
-          # Misc
-          # pkgs.cowsay # Cowsay reborn, written in Go
-          # pkgs.irssi # Terminal based IRC client
-          # pkgs.mutagen # Make remote development work with your local tools
-          # pkgs.mutagen-compose # Compose with Mutagen integration
-          # pkgs.pv # A Wave-to-Notes transcriber
-          # Sort these
-          # pkgs.cfssl # Cloudflare's PKI and TLS toolkit
-          # pkgs.httpie # A command line HTTP client whose goal is to make CLI human-friendly
-          # pkgs.nats-server # High-Performance server for NATS
-          # pkgs.natscli # NATS Command Line Interface
-          # pkgs.neovim # Vim text editor fork focused on extensibility and agility
-          # pkgs.redis # An open source, advanced key-value store
-          # pkgs.bitwarden-cli # A secure and free password manager for all of your devices
-          # pkgs.vector # A high-performance observability data pipeline
-          # pkgs.watchman # Watches files and takes action when they change
         ]
         ++ lib.optional config.home.modules.ai [
           pkgs.master.claude-code # Claude AI coding assistant
           pkgs.codex # OpenAI Codex CLI
           pkgs.master.opencode # Open source AI coding assistant
           pkgs.master.pi-coding-agent # Minimal terminal coding harness.
-          # pkgs.crush # AI coding assistant
           pkgs.worktrunk # AI coding assistant
           pkgs.master.herdr # Terminal multiplexer for AI coding agents
         ]
@@ -348,6 +317,46 @@ in {
           pkgs.openocd # On-Chip Debugging, In-System Programming and Boundary-Scan Testing for Embedded Target Devices
           pkgs.picotool # Tool for interacting with RP-series device(s) in BOOTSEL mode, or with an RP-series binary
           pkgs.gcc-arm-embedded # GNU Arm Embedded Toolchain
+        ]
+        ++ lib.optional config.home.modules.testing [
+          # macOS
+          # pkgs.gnupg # Managed by programs.gpg
+          # pkgs.lima # Linux virtual machines (on macOS, in most cases)
+          # pkgs.mitmproxy # Man-in-the-middle proxy
+          # pkgs.darwin.libiconv # required for -liconv mitmproxy compilation
+          # Minimal
+          # pkgs.master.gitui # Blazing fast terminal-ui for Git written in Rust
+          # pkgs.nixpkgs-fmt # Nix code formatter for nixpkgs
+          # pkgs.direnv # Managed by programs.direnv
+          # pkgs.jq # Managed by programs.jq
+          # pkgs.lazygit # Managed by programs.lazygit
+          # pkgs.ripgrep # Managed by programs.ripgrep
+          # Developer
+          # pkgs.master.cargo-watch # A Cargo subcommand for watching over Cargo project's source
+          # pkgs.gh # Managed by programs.gh
+          # pkgs.btop # Managed by programs.btop
+          # pkgs.s-tui # Stress-Terminal UI monitoring tool - disabled because marked as broken
+          # pkgs.nodePackages_latest.aws-cdk # CDK Toolkit, the command line tool for CDK apps
+          # pkgs.yubikey-manager # Command line tool for configuring any YubiKey over all USB transports
+          # pkgs.yubikey-personalization # A library and command line tool to personalize YubiKeys
+          # Hipster
+          # pkgs.cowsay # Cowsay reborn, written in Go
+          # pkgs.irssi # Terminal based IRC client
+          # pkgs.mutagen # Make remote development work with your local tools
+          # pkgs.mutagen-compose # Compose with Mutagen integration
+          # pkgs.pv # A Wave-to-Notes transcriber
+          # pkgs.cfssl # Tools to bootstrap CAs, certificate requests, and certificates
+          # pkgs.httpie # A command line HTTP client whose goal is to make CLI human-friendly
+          # pkgs.nats-server # High-Performance server for NATS
+          # pkgs.natscli # NATS Command Line Interface
+          # pkgs.neovim # Vim text editor fork focused on extensibility and agility
+          # pkgs.redis # An open source, advanced key-value store
+          # pkgs.bitwarden-cli # A secure and free password manager for all of your devices
+          # pkgs.vector # A high-performance observability data pipeline
+          # pkgs.watchman # Watches files and takes action when they change
+          # AI
+          # pkgs.crush # AI coding assistant
+          # Embedded
           # pkgs.gdb # The GNU Project debugger
         ]
         ++ lib.optional config.home.modules.modelling [
